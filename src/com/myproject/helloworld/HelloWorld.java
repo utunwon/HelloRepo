e5118dcf28c6f7504f093fe8ec932dcfc168a0b9
utunwon <utunwon@gmail.com>
2015-07-15 23:56:52 -0400

    initial check in

---
+++ src/com/myproject/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.myproject.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello hello");
+		System.out.println("Hello hello");
+
+	}
+
+}
